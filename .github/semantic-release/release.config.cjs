ea49a7ae8845fdbe062fc17975ea39b417681913
Lens <yoanyombapro@gmail.com>
2026-08-15 00:32:19 -0400

    build(release): add semantic-release tooling

---
+++ .github/semantic-release/release.config.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  branches: ["main"],
+  tagFormat: "v${version}",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    ["@semantic-release/npm", { npmPublish: false }],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json", "package-lock.json"],
+        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+    [
+      "@semantic-release/github",
+      {
+        draftRelease: true,
+        successComment: false,
+        failComment: false,
+        releasedLabels: false,
+      },
+    ],
+  ],
+};
